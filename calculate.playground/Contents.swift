ae0f3508b3632fc5392a94ae11a110e2925f3a48
Hiromu Tsuruta <hiromutsuruta@yahoo.co.jp>
2019-09-05 18:20:20 +0900

    append Q2

--- calculate.playground/Contents.swift
+++ calculate.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-/* 0の連番が最大値を返す */
+/* Q1 0の連番が最大値を返す */
 
 /* 頭が０からの場合は丸め込まれる想定 */
 
@@ -26,3 +26,16 @@ func takeOutMaxCountOfLinedUpZero(from indexes: Int) -> Int {
 takeOutMaxCountOfLinedUpZero(from: 110001101) // 3
 takeOutMaxCountOfLinedUpZero(from: 1100010000010001) // 5
 takeOutMaxCountOfLinedUpZero(from: 0001100010000) // 4
+
+
+
+/* Q2 ２つを取り出して合計8を作れるかどうか */
+
+func hasPair(in data: [Int]) -> Bool {
+    return data.enumerated().first { offset, index in
+        return data.enumerated().first { $0.offset != offset && $0.element + index == 8 } != nil
+    } != nil
+}
+
+hasPair(in: [1,2,3,9])
+hasPair(in: [1,2,4,4])
