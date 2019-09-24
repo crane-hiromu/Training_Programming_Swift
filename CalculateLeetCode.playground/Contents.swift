087fbbc2b7a8d8ff5cac5e64a8f390bd1f05e8d0
h.crane <h.crane.t@gmail.com>
2019-09-25 03:09:26 +0900

    append result

--- CalculateLeetCode.playground/Contents.swift
+++ CalculateLeetCode.playground/Contents.swift
@@ -79,3 +79,52 @@ canCloseCharacters("([)]") // false
 canCloseCharacters("{[]}") // true
 canCloseCharacters("") // true
 canCloseCharacters("]]") // true
+
+
+// https://leetcode.com/problems/kth-largest-element-in-a-stream
+// Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+
+// Example:
+//
+//    int k = 3;
+//    int[] arr = [4,5,8,2];
+//    KthLargest kthLargest = new KthLargest(3, arr);
+//    kthLargest.add(3);   // returns 4
+//    kthLargest.add(5);   // returns 5
+//    kthLargest.add(10);  // returns 5
+//    kthLargest.add(9);   // returns 8
+//    kthLargest.add(4);   // returns 8
+//
+// Note: You may assume that nums' length ≥ k-1 and k ≥ 1.
+
+final class KthLargest {
+    
+    private let k: Int
+    private var nums: [Int]
+
+    init(_ k: Int, _ nums: [Int]) {
+        self.k = k
+        self.nums = nums.sorted { $1 < $0 }
+    }
+    
+    func add(_ val: Int) -> Int {
+        self.nums.append(val)
+        
+        for i in 0...nums.count-1 {
+            if nums[i] < val {
+                nums.insert(val, at: i)
+                break
+            }
+        }
+        return self.nums[self.k-1]
+    }
+}
+
+let arr: [Int] = [4,5,8,2]
+let kth = KthLargest(3, arr)
+kth.add(3)
+kth.add(5)
+kth.add(10)
+kth.add(9)
+kth.add(4)
