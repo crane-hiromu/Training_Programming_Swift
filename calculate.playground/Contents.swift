2e86d86f6f0a66b5434f97ee1d050595219a0223
h.crane <h.crane.t@gmail.com>
2020-02-18 04:07:00 +0900

    sppend

--- calculate.playground/Contents.swift
+++ calculate.playground/Contents.swift
@@ -21,10 +21,10 @@ func takeOutMaxCountOfLinedUpZero(from indexes: Int) -> Int {
     
     return max
 }
-
-takeOutMaxCountOfLinedUpZero(from: 110001101) // 3
-takeOutMaxCountOfLinedUpZero(from: 1100010000010001) // 5
-takeOutMaxCountOfLinedUpZero(from: 0001100010000) // 4
+//
+//takeOutMaxCountOfLinedUpZero(from: 110001101) // 3
+//takeOutMaxCountOfLinedUpZero(from: 1100010000010001) // 5
+//takeOutMaxCountOfLinedUpZero(from: 0001100010000) // 4
 
 
 
@@ -35,10 +35,10 @@ func hasPair(in data: [Int]) -> Bool {
         return data.enumerated().first { $0.offset != offset && $0.element + index == 8 } != nil
     } != nil
 }
-
-hasPair(in: [1,2,3,9]) // false
-hasPair(in: [1,2,4,4]) // true
-
+//
+//hasPair(in: [1,2,3,9]) // false
+//hasPair(in: [1,2,4,4]) // true
+//
 
 
 
@@ -57,56 +57,124 @@ func sortInt(_ items: [Int]) -> [Int] {
     }
     return sortedItems
 }
-
-func sortArray() {
-    let array = Array(1...100)
-    
-    var first = array
-    var second = array
-    var third = array
-    
-    let startTime1 = CFAbsoluteTimeGetCurrent()
-    first.sorted()
-    let timeElapsed1 = CFAbsoluteTimeGetCurrent() - startTime1
-    print("1: \(timeElapsed1) seconds")
-    print(first)
-    
-    
-    let startTime2 = CFAbsoluteTimeGetCurrent()
-    sortInt(second)
-    let timeElapsed2 = CFAbsoluteTimeGetCurrent() - startTime2
-    print("2: \(timeElapsed2) seconds")
-    print(second)
-    
-    
-//    let startTime3 = CFAbsoluteTimeGetCurrent()
-////    let hh = second.withContiguousMutableStorageIfAvailable { $0.baseAddress!.pointee }
 //
-//    let timeElapsed3 = CFAbsoluteTimeGetCurrent() - startTime3
-//    print("3: \(timeElapsed2) seconds")
-//    print(third)
-    
-}
-
-sortArray()
-
-
-
+//func sortArray() {
+//    let array = Array(1...100)
 //
-//func swapByPointer<T>(x: UnsafeMutablePointer<T>, y: UnsafeMutablePointer<T>) {
-//    let z: T = x.pointee
-//    x.pointee = y.pointee
-//    y.pointee = z
+//    var first = array
+//    var second = array
+//    var third = array
+//
+//    let startTime1 = CFAbsoluteTimeGetCurrent()
+//    first.sorted()
+//    let timeElapsed1 = CFAbsoluteTimeGetCurrent() - startTime1
+//    print("1: \(timeElapsed1) seconds")
+//    print(first)
+//
+//
+//    let startTime2 = CFAbsoluteTimeGetCurrent()
+//    sortInt(second)
+//    let timeElapsed2 = CFAbsoluteTimeGetCurrent() - startTime2
+//    print("2: \(timeElapsed2) seconds")
+//    print(second)
+//
+//
+////    let startTime3 = CFAbsoluteTimeGetCurrent()
+//////    let hh = second.withContiguousMutableStorageIfAvailable { $0.baseAddress!.pointee }
+////
+////    let timeElapsed3 = CFAbsoluteTimeGetCurrent() - startTime3
+////    print("3: \(timeElapsed2) seconds")
+////    print(third)
+//
 //}
 //
-//var a: Int = 10
-//var b: Int = 20
-//
-//// a = 10, b = 20
-//print("a = \(a), b = \(b)")
-//
-//// Swap
-//swapByPointer(&a, &b)
-//
-//// a = 20, b = 10
-//print("a = \(a), b = \(b)")
+//sortArray()
+
+
+// pointer and input
+
+// 1
+func swapByPointer<T>(x: UnsafeMutablePointer<T>, y: UnsafeMutablePointer<T>) {
+    let z: T = x.pointee
+    x.pointee = y.pointee
+    y.pointee = z
+}
+func execSwapByPointer() {
+    let st = CFAbsoluteTimeGetCurrent()
+    
+    var a: Int = 10
+    var b: Int = 20
+    swapByPointer(x: &a, y: &b)
+
+    let et = CFAbsoluteTimeGetCurrent() - st
+    print("\(et) seconds")
+}
+
+// 2
+func swapIntByPointer(x: UnsafeMutablePointer<Int>, y: UnsafeMutablePointer<Int>) {
+    let z = x.pointee
+    x.pointee = y.pointee
+    y.pointee = z
+}
+func execSwapIntByPointer() {
+    let st = CFAbsoluteTimeGetCurrent()
+    
+    var c: Int = 10
+    var d: Int = 20
+    swapIntByPointer(x: &c, y: &d)
+
+    let et = CFAbsoluteTimeGetCurrent() - st
+    print("\(et) seconds")
+}
+
+// 3
+func swapByInout<T>(x: inout T, y: inout T) {
+    let z: T = x
+    x = y
+    y = z
+}
+func execSwapByInout() {
+    let st = CFAbsoluteTimeGetCurrent()
+    
+    var e: Int = 10
+    var f: Int = 20
+    swapByInout(x: &e, y: &f)
+
+    let et = CFAbsoluteTimeGetCurrent() - st
+    print("\(et) seconds")
+}
+
+// 4
+func swapIntByInout(x: inout Int, y: inout Int) {
+    let z = x
+    x = y
+    y = z
+}
+func execSwapIntByInout() {
+    let st = CFAbsoluteTimeGetCurrent()
+    
+    var g: Int = 10
+    var h: Int = 20
+    swapIntByInout(x: &g, y: &h)
+
+    let et = CFAbsoluteTimeGetCurrent() - st
+    print("\(et) seconds")
+}
+
+// exec
+execSwapByPointer()
+execSwapByPointer()
+execSwapByPointer()
+
+execSwapIntByPointer()
+execSwapIntByPointer()
+execSwapIntByPointer()
+
+execSwapByInout()
+execSwapByInout()
+execSwapByInout()
+
+execSwapIntByInout()
+execSwapIntByInout()
+execSwapIntByInout()
+
