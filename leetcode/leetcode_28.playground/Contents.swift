15fa6dff2ac0062bb0ff70e389ca1003d59450b3
h.crane <h.crane.t@gmail.com>
2020-06-14 00:31:48 +0900

    fixed result

--- leetcode/leetcode_28.playground/Contents.swift
+++ leetcode/leetcode_28.playground/Contents.swift
@@ -23,3 +23,26 @@ func strStr(_ haystack: String, _ needle: String) -> Int {
     
     return count
 }
+
+/// answer
+func strStr2(_ haystack: String, _ needle: String) -> Int {
+    if haystack.count == needle.count {
+        return haystack != needle ? -1 : 0
+    } else if needle.isEmpty {
+        return 0
+    }
+    
+    let count = haystack.components(separatedBy: needle)[0].count
+    return count != haystack.count ? count : -1
+}
+
+/// from discuss interresting answer
+func strStr3(_ haystack: String, _ needle: String) -> Int {
+    if haystack.count == needle.count {
+        return haystack != needle ? -1 : 0
+    } else if let index = haystack.range(of: needle) {
+        return haystack.distance(from: haystack.startIndex, to: index.lowerBound)
+    } else {
+        return needle.isEmpty ? 0 : -1
+    }
+}
