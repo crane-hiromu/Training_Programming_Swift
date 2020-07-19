4b93d0fff4110325de89abab8572450b7526458f
h.crane <h.crane.t@gmail.com>
2020-07-19 14:16:34 +0900

    append sort

--- leetcode/leetcode_ 169.playground/Contents.swift
+++ leetcode/leetcode_ 169.playground/Contents.swift
@@ -13,7 +13,7 @@ import UIKit
  Output: 2
  */
 
-/// answer1 (linear serach)
+/// answer1 ( hashmap )
 func majorityElement(_ nums: [Int]) -> Int {
     guard !nums.isEmpty else { return 0 }
     
@@ -28,4 +28,13 @@ func majorityElement(_ nums: [Int]) -> Int {
     }
 
     return counter.sorted { $1.value < $0.value }[0].key
+}
+
+/// answer2 ( sort )
+/// 問題が「more than ⌊ n/2 ⌋ times.」のため、最も多いものは必ず半分以上あるためこの手法が使える。
+/// 単調増加になっていること表せれば良いので、ソートの部分はこの問題ではあまり重要ではない。
+/// -> 本来はsortedを使わずソートのコードを探究すべし
+func majorityElement2(_ nums: [Int]) -> Int {
+    let array = nums.sorted()
+    return array[array.count/2]
 }
