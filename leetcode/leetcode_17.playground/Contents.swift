e369c6eb86f7fd8aff2f6eed0ae7bbd67b6a6b62
h.crane <h.crane.t@gmail.com>
2020-07-20 19:59:30 +0900

    appned simple result

---
+++ leetcode/leetcode_17.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+/*
+ Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+ A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+ Example:
+ Input: "23"
+ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ 
+ Note:
+ Although the above answer is in lexicographical order, your answer could be in any order you want.
+ */
+
+/// answers ( backtracking )
+let numbers: [String: [String]] = [
+    "2": ["a","b","c"],
+    "3": ["d","e","f"],
+    "4": ["g","h","i"],
+    "5": ["j","k","l"],
+    "6": ["m","n","o"],
+    "7": ["p","q","r","s"],
+    "8": ["t","u","v"],
+    "9": ["w","x","y","z"]
+]
+
+func letterCombinations1(_ digits: String) -> [String] {
+    guard !digits.isEmpty, !digits.contains("1") else { return [] } // ignore empty
+    guard digits.count != 1 else { return numbers[digits]! } // ignore single number
+    
+    let letters = digits.compactMap { numbers[String($0)] }
+    var array = Array(letters[0..<letters.count])
+    var result = array[0]
+    
+    while 1 < array.count {
+        let first = result
+        let second = array[1]
+        var combination = [String]()
+        
+        for s1 in first {
+            for s2 in second {
+                combination.append("\(s1)\(s2)")
+            }
+        }
+        array = Array(array.dropFirst())
+        result = combination
+    }
+    return result
+}
