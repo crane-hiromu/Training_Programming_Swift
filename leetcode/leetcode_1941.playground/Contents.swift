3de4f921a6021605af5465a2d7ec199f5b941b35
h.crane <h.crane.t@gmail.com>
2021-08-15 00:40:44 +0900

    feat: leetcode1941

---
+++ leetcode/leetcode_1941.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+/*
+ Given a string s, return true if s is a good string, or false otherwise.
+ A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+ Example 1:
+ Input: s = "abacbc"
+ Output: true
+ Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+ 
+ Example 2:
+ Input: s = "aaabb"
+ Output: false
+ Explanation: The characters that appear in s are 'a' and 'b'.
+ 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+  
+ Constraints:
+ 1 <= s.length <= 1000
+ */
+
+func areOccurrencesEqual(_ s: String) -> Bool {
+    let values = s.reduce(into: [Character: Int]()) { $0[$1] = ($0[$1] ?? 0) + 1 }.values
+    
+    var last: Int?
+    for val in values {
+        if let l = last, l != val {
+            return false
+        }
+        last = val
+    }
+    return true
+}
