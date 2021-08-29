ad0f1ad0e39826636c378f3c41cdad63b525f0f7
h.crane <h.crane.t@gmail.com>
2021-08-29 13:56:07 +0900

    feat: leetcode1984

---
+++ leetcode/leetcode_1984.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+/*
+ You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+
+ Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+
+ Return the minimum possible difference.
+
+ Example 1:
+ Input: nums = [90], k = 1
+ Output: 0
+ Explanation: There is one way to pick score(s) of one student:
+ - [90]. The difference between the highest and lowest score is 90 - 90 = 0.
+ The minimum possible difference is 0.
+ 
+ Example 2:
+ Input: nums = [9,4,1,7], k = 2
+ Output: 2
+ Explanation: There are six ways to pick score(s) of two students:
+ - [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
+ - [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
+ - [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
+ - [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
+ - [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
+ - [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
+ The minimum possible difference is 2.
+  
+ Constraints:
+ 1 <= k <= nums.length <= 1000
+ 0 <= nums[i] <= 105
+ */
+
+func minimumDifference(_ nums: [Int], _ k: Int) -> Int {
+    let sortedNums = nums.sorted()
+    var result = sortedNums[k-1]-sortedNums[0]
+    var i = 0
+    for j in (k-1)..<sortedNums.count {
+        result = min(result, sortedNums[j]-sortedNums[i])
+        i += 1
+    }
+    
+    return result
+}
