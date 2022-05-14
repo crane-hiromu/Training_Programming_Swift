5e6dd83c162cba9b9b7a487ddd2133691abcf83f
crane-hiromu <h.crane.t@gmail.com>
2022-05-14 12:47:16 +0900

    feat: leetcode2264

---
+++ leetcode/leetcode_2264.playground/Contents.swift
@@ -0,0 +1,48 @@
+/*
+ You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+ It is a substring of num with length 3.
+ It consists of only one unique digit.
+ Return the maximum good integer as a string or an empty string "" if no such integer exists.
+
+ Note:
+ A substring is a contiguous sequence of characters within a string.
+ There may be leading zeroes in num or a good integer.
+
+ Example 1:
+ Input: num = "6777133339"
+ Output: "777"
+ Explanation: There are two distinct good integers: "777" and "333".
+ "777" is the largest, so we return "777".
+ 
+ Example 2:
+ Input: num = "2300019"
+ Output: "000"
+ Explanation: "000" is the only good integer.
+ 
+ Example 3:
+ Input: num = "42352338"
+ Output: ""
+ Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+  
+ Constraints:
+ 3 <= num.length <= 1000
+ num only consists of digits.
+ */
+
+func largestGoodInteger(_ num: String) -> String {
+    var last = Character(" ")
+    var maximum = -1
+    var counter = 0
+    
+    for char in num {
+        counter = (last == char) ? counter + 1 : 1
+        last = char
+        
+        if counter == 3 {
+            maximum = max(maximum, Int(String(char))!)
+        }
+    }
+    
+    return maximum < 0 ? "" : "\(maximum)\(maximum)\(maximum)"
+}
