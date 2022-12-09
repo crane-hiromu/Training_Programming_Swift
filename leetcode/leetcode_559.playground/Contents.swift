eaa18732541c334e99513a3120b33eb10af567b7
crane-hiromu <h.crane.t@gmail.com>
2022-12-10 06:00:52 +0900

    feat: leetcode559

---
+++ leetcode/leetcode_559.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+/*
+ Given a n-ary tree, find its maximum depth.
+ The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ Example 1:
+ Input: root = [1,null,3,2,4,null,5,6]
+ Output: 3
+ 
+ Example 2:
+ Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ Output: 5
+  
+ Constraints:
+ The total number of nodes is in the range [0, 104].
+ The depth of the n-ary tree is less than or equal to 1000.
+ */
+
+public class Node {
+    public var val: Int
+    public var children: [Node]
+    public init(_ val: Int) {
+        self.val = val
+        self.children = []
+    }
+}
+
+// DFS
+class Solution {
+    func maxDepth(_ root: Node?) -> Int {
+        guard let r = root else { return 0 }
+
+        var maximum = 0
+        preorder(next: r, depth: 1, maximum: &maximum)
+        return maximum
+    }
+
+    func preorder(next node: Node, depth: Int, maximum: inout Int) {
+        maximum = max(depth, maximum)
+
+        for child in node.children {
+            preorder(next: child, depth: depth+1, maximum: &maximum)
+        }
+    }
+}
