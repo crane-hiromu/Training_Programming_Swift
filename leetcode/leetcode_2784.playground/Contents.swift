215fc08184787fa1274ee80e1de076d18771b005
crane-hiromu <h.crane.t@gmail.com>
2023-11-05 18:28:54 +0900

    add 2784

---
+++ leetcode/leetcode_2784.playground/Contents.swift
@@ -0,0 +1,44 @@
+/*
+ You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+ base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+
+ Return true if the given array is good, otherwise return false.
+ Note: A permutation of integers represents an arrangement of these numbers.
+ 
+ Example 1:
+ Input: nums = [2, 1, 3]
+ Output: false
+ Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. However, base[3] has four elements but array nums has three. Therefore, it can not be a permutation of base[3] = [1, 2, 3, 3]. So the answer is false.
+ 
+ Example 2:
+ Input: nums = [1, 3, 3, 2]
+ Output: true
+ Explanation: Since the maximum element of the array is 3, the only candidate n for which this array could be a permutation of base[n], is n = 3. It can be seen that nums is a permutation of base[3] = [1, 2, 3, 3] (by swapping the second and fourth elements in nums, we reach base[3]). Therefore, the answer is true.
+ 
+ Example 3:
+ Input: nums = [1, 1]
+ Output: true
+ Explanation: Since the maximum element of the array is 1, the only candidate n for which this array could be a permutation of base[n], is n = 1. It can be seen that nums is a permutation of base[1] = [1, 1]. Therefore, the answer is true.
+ 
+ Example 4:
+ Input: nums = [3, 4, 4, 1, 2, 1]
+ Output: false
+ Explanation: Since the maximum element of the array is 4, the only candidate n for which this array could be a permutation of base[n], is n = 4. However, base[4] has five elements but array nums has six. Therefore, it can not be a permutation of base[4] = [1, 2, 3, 4, 4]. So the answer is false.
+ */
+
+func isGood(_ nums: [Int]) -> Bool {
+    var maximum = Int.min, dict = [Int: Int]()
+    for num in nums {
+        maximum = max(maximum, num)
+        dict[num, default: 0] += 1
+    }
+
+    for i in 1...maximum {
+        if i == maximum, dict[i] != 2 {
+            return false
+        } else if i != maximum, dict[i] != 1 {
+            return false
+        }
+    }
+    return true
+}
