96fd6a5fc663e01238990f9487162cb4ff31443a
hcrane <hiromutsuruta@yahoo.co.jp>
2024-01-08 19:55:22 +0900

    100035

---
+++ leetcode/leetcode_10035.playground/Contents.swift
@@ -0,0 +1,41 @@
+/*
+You are given a 2D 0-indexed integer array dimensions.
+
+For all indices i, 0 <= i < dimensions.length, dimensions[i][0] represents the length and dimensions[i][1] represents the width of the rectangle i.
+
+Return the area of the rectangle having the longest diagonal. If there are multiple rectangles with the longest diagonal, return the area of the rectangle having the maximum area.
+
+Example 1:
+Input: dimensions = [[9,3],[8,6]]
+Output: 48
+Explanation: 
+For index = 0, length = 9 and width = 3. Diagonal length = sqrt(9 * 9 + 3 * 3) = sqrt(90) ≈ 9.487.
+For index = 1, length = 8 and width = 6. Diagonal length = sqrt(8 * 8 + 6 * 6) = sqrt(100) = 10.
+So, the rectangle at index 1 has a greater diagonal length therefore we return area = 8 * 6 = 48.
+
+Example 2:
+Input: dimensions = [[3,4],[4,3]]
+Output: 12
+Explanation: Length of diagonal is the same for both which is 5, so maximum area = 12.
+ 
+Constraints:
+1 <= dimensions.length <= 100
+dimensions[i].length == 2
+1 <= dimensions[i][0], dimensions[i][1] <= 100
+*/
+
+func areaOfMaxDiagonal(_ dimensions: [[Int]]) -> Int {
+    var maximum: Double = 0.0, result = 0
+
+    for dimension in dimensions {
+        let root = sqrt(pow(Double(dimension[0]), 2.0)+pow(Double(dimension[1]), 2.0))
+
+        if maximum < root {
+            maximum = root
+            result = dimension[0]*dimension[1]
+        } else if maximum == root {
+            result = max(result, dimension[0]*dimension[1])
+        }
+    }
+    return result
+}
